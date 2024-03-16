35462794664f842bce752719b0f053ec94bc66f3
kanary <kanary@adminui-MacBookPro.local>
2024-03-16 16:27:36 +0900

    initial logic

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [scriptInjected, setScriptInjected] = useState(false);
+
+  useEffect(() => {
+    async function init() {
+      const initialValue = await browser.runtime.sendMessage({
+        action: "getToggleState",
+      });
+      console.log("initialValue", initialValue);
+      setScriptInjected(initialValue.isEnabled);
+    }
+    init();
+  }, []);
+
+  const toggleScriptInjection = () => {
+    browser.runtime.sendMessage({
+      action: "toggleState",
+      isEnabled: !scriptInjected,
+    });
+    setScriptInjected(!scriptInjected);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={toggleScriptInjection}>
+        {scriptInjected ? "Injected" : "Not Injected"}
+      </button>
+    </div>
   );
 }
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'wxt';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "wxt";
+import react from "@vitejs/plugin-react";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   vite: () => ({
     plugins: [react()],
   }),
+  manifest: {
+    host_permissions: ["*://github.com/*"],
+    permissions: ["storage", "scripting"],
+  },
 });
